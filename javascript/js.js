eb44a56d125e19495746061fd6a53b993a50a71d
Fennema lucas <ld.fennema@gmail.com>
2020-07-21 19:40:21 -0300

    falta modificar la vista de las cards

--- javascript/js.js
+++ javascript/js.js
@@ -1,16 +1,31 @@
+
+function formSender(event){
+  event.preventDefault()
+  let mail = Object.values(event.target).reduce((obj,field) => { obj[field.name] = field.value; return obj }, {})
+  let httpRequest = new XMLHttpRequest()
+
+  httpRequest.open('POST','https://git.heroku.com/kanban-api-node.git',mail)
+  httpRequest.send()
+  
+  window.reload()
+}
+
+const form = document.getElementById('messageForm');
+form.addEventListener('submit', formSender);
+
+
 function uncheck() {
-  document.getElementById("menu-btn").checked = false;
+  document.getElementById("menu-btn").checked = false
 }
 
 function scroll2Proyects(classdiv) {
   /*close menu*/
-  let place = document.getElementById(classdiv);
-  uncheck();
+  let place = document.getElementById(classdiv)
+  uncheck()
   if (screen.width <= 768) {
     /* 48em resolution*/
-    setTimeout(() => place.scrollIntoView(), 210);
+    setTimeout(() => place.scrollIntoView(), 210)
   } else {
-    place.scrollIntoView();
+    place.scrollIntoView()
   }
 }
-
